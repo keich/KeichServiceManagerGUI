33bdc015019ba5d3bb41fdc98f1ded97d25748be
Зажигин Александр Сергеевич <ZazhiginAS@corp.lukoil.com>
2024-05-08 16:54:36 +0300

    add filter by status

--- src/components/EventsTable.vue
+++ src/components/EventsTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-	import { shallowRef, ref, onMounted, onBeforeUnmount, watch, defineProps } from 'vue'
+	import { shallowRef, ref, onMounted, onBeforeUnmount, watch, defineProps, computed } from 'vue'
 	import type { IEvent} from "@/types/IEvent.ts"
 	import type { TreeNode }  from 'primevue/treenode'
 	import ItemRepository from '@/api/ItemRepository.ts'
@@ -33,16 +33,33 @@
 	const headerDialog = ref<string>()
 	const dataDialog = ref<TreeNode[]>()
 	
-	let refreshEventsInterval: number = null
+	interface ICounter {
+    	clear: number,
+    	indeterminate: number,
+    	info: number,
+    	warning: number,
+    	major: number,
+    	critical: number,
+    	all: number
+	} 
 	
-	let searchTimer = null
+	function getClearCounters() {
+		return {clear: 0, indeterminate: 0, info: 0, warning: 0, major: 0, critical: 0, all: 0}
+	}
+	
+	const counters = ref<ICounter>(getClearCounters())
+	
+	let refreshEventsInterval: number
+	
+	let searchTimer: number | undefined = undefined
 
 	const filters = ref({
-	    global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+	    global: { value: '', matchMode: FilterMatchMode.CONTAINS },
+		'data.status': { value: '', matchMode: FilterMatchMode.CONTAINS }
 	})
 	
 	const columns  = ref([
-		{ field: 'data.status', header: 'Status', class: "white-space-normal p-1 w-1 pl-2"},
+		{ field: 'data.status', filterField:'data.status', header: 'Status', class: "white-space-normal p-1 w-1 pl-2"},
 		{ field: 'data.toNow', header: 'To Now' , class: "white-space-normal p-1 w-2 pl-2"},
 		{ field: 'data.fields.node', header: 'Node', class: "white-space-normal p-1 w-3 pl-2"},
 		{ field: 'data.fields.summary', header: 'Summary' , class: "white-space-normal p-1 w-4  pl-2"}
@@ -72,6 +89,30 @@
 					}
 					return intDiff
 				})
+			let tmp = getClearCounters()
+			events.value.forEach((evt) => {
+				switch(evt.data.status){
+				case 'CLEAR':
+					tmp.clear++
+					break
+				case "INFORMATION":
+					tmp.info++
+					break
+				case "INDETERMINATE":
+					tmp.indeterminate++
+					break
+				case "WARNING":
+					tmp.warning++
+					break
+				case "MAJOR":
+					tmp.major++
+					break
+				case "CRITICAL":
+					tmp.critical++
+					break
+				}
+			})
+			counters.value = tmp;
 		})
 		.catch(error => { 
 			console.log("Error ",error)
@@ -110,15 +151,34 @@
 		isShowDialog.value = true
 	}
 	
-	function onFilterGlobal(event){
+	function onFilterGlobal(event: any){
 	    if (searchTimer){
 	        clearTimeout(searchTimer)
-	        searchTimer = null
+	        searchTimer = undefined
 	    }
 	    searchTimer = setTimeout(() => {
 	    	filters.value['global'].value = event.target.value.trim()
 	    }, 800)
 	}
+	
+	function onClickFilterStatus(val: string ) {
+		if(val == null){
+			filters.value['data.status'].value = ''	
+		} else {
+			filters.value['data.status'].value = val
+		}
+	}
+	
+	const buttons = computed(() => {
+		return [
+		    { show: counters.value.clear > 0, status: 'CLEAR', value: 'CLEAR', text: 'Clear(' + counters.value.clear + ')' },
+		    { show: counters.value.indeterminate > 0, status: 'INDETERMINATE', value: 'INDETERMINATE', text: 'Indeterminate(' + counters.value.indeterminate + ')' },
+		    { show: counters.value.info > 0, status: 'INFORMATION', value: 'INFORMATION', text: 'Info(' + counters.value.info + ')' },
+		    { show: counters.value.warning > 0, status: 'WARNING', value: 'WARNING', text: 'Warning(' + counters.value.warning + ')' },
+		    { show: counters.value.major > 0, status: 'MAJOR', value: 'MAJOR', text: 'Major(' + counters.value.major + ')' },
+		    { show: counters.value.critical > 0, status: 'CRITICAL', value: 'CRITICAL', text: 'Critical(' + counters.value.critical + ')' },
+		    { show: true, status: 'GRAY', value: '', text: 'All events(' + counters.value.all + ')'  } ]
+	})
 </script>
 
 <template>
@@ -126,11 +186,13 @@
   	<DataTable class="flex flex-column h-full overflow-hidden" paginator :rows="50" :rowsPerPageOptions="[50, 100, 1000]" 
   	    :value="events" :loading="loading" removableSort @dblclick="onRowDblClick"  @update:selection="onNodeSelect" 
   	    :metaKeySelection="false" selectionMode="single" tableStyle="min-width: 50rem" dataKey="key" v-model:filters="filters" >
-		<template #header >
-			<div class="text-right">
-				<div class="" >
-					<i class="pi pi-search m-2"></i>
-					<InputText @input="onFilterGlobal" placeholder="Global Search" class="p-1"/>
+		<template #header>
+			<div class="align-items-center flex p-0 m-0">
+				<div v-for="button in buttons" class="cursor-pointer">
+					<StatusIcon v-if="button.show" :status="button.status" class="m-1" @click="onClickFilterStatus(button.value)" :text="button.text" />
+				</div>
+				<div class="flex m-1" >
+					<i class="pi pi-search m-2"></i><InputText @input="onFilterGlobal" placeholder="Global Search" class="p-1"/>
 				</div>
 			</div>
 		</template>
@@ -140,6 +202,7 @@
 			</template>
 	    </Column>
 	</DataTable>
+
 	<Dialog v-model:visible="isShowDialog"  :header="headerDialog" :style="{ width: '70vw' }">
         <TreeTable :value="dataDialog" class="eventDetails">
             <Column field="name" header="Field" expander></Column>
