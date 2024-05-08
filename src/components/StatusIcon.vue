a5a26fd00eadccc9777b5834dfb3ca93878cd5db
Зажигин Александр Сергеевич <ZazhiginAS@corp.lukoil.com>
2024-05-08 15:42:42 +0300

    add border, nowrap, add gray

--- src/components/StatusIcon.vue
+++ src/components/StatusIcon.vue
@@ -24,12 +24,13 @@
 	})
 
 const classesConfig: IStatusHash = {
-	'CLEAR': {'iconStyle':'color: var(--green-600)', 'iconClass': 'pi-check-circle', 'backgroundClass': 'bg-green-50'},
-	'INDETERMINATE':{'iconStyle':'color: var(--purple-600)','iconClass': 'pi-question-circle', 'backgroundClass': 'bg-purple-50'},
-	'INFORMATION': {'iconStyle':'color: var(--blue-500)','iconClass': 'pi-info-circle', 'backgroundClass': 'bg-blue-50'},
-	'WARNING':{'iconStyle':'color: var(--orange-600)','iconClass': 'pi-exclamation-circle', 'backgroundClass': 'bg-orange-50'},
-	'MAJOR': {'iconStyle':'color: var(--orange-600)','iconClass': 'pi-exclamation-triangle', 'backgroundClass': 'bg-orange-50'},
-	'CRITICAL': {'iconStyle':'color: var(--red-600)','iconClass': 'pi-times-circle', 'backgroundClass': 'bg-red-100'}
+	'CLEAR': {'iconStyle':'color: var(--green-600)', 'iconClass': 'pi-check-circle', 'backgroundClass': 'border-gray-200 bg-green-50'},
+	'INDETERMINATE':{'iconStyle':'color: var(--purple-600)','iconClass': 'pi-question-circle', 'backgroundClass': 'border-gray-200 bg-purple-50'},
+	'INFORMATION': {'iconStyle':'color: var(--blue-500)','iconClass': 'pi-info-circle', 'backgroundClass': 'border-gray-200 bg-blue-50'},
+	'WARNING':{'iconStyle':'color: var(--orange-600)','iconClass': 'pi-exclamation-circle', 'backgroundClass': 'border-gray-200 bg-orange-50'},
+	'MAJOR': {'iconStyle':'color: var(--orange-600)','iconClass': 'pi-exclamation-triangle', 'backgroundClass': 'border-gray-200 bg-orange-50'},
+	'CRITICAL': {'iconStyle':'color: var(--red-600)','iconClass': 'pi-times-circle', 'backgroundClass': 'border-gray-200 bg-red-100'},
+	'GRAY': {'iconStyle':'color: var(--surface-900)','iconClass': 'pi-times-circle', 'backgroundClass': 'surface-300'}
 }		
 	
 const statusClasses = computed<IStatus>(() => {
@@ -43,9 +44,9 @@ const statusClasses = computed<IStatus>(() => {
 </script>
 
 <template>
-	<div :class="['h-full border-round-md min-w-min', statusClasses.backgroundClass]">
+	<div :class="['align-items-center border-1 flex justify-content-center h-full border-round-md min-w-min', statusClasses.backgroundClass]">
 		<span :class="['m-2 pi', statusClasses.iconClass]" :style="statusClasses.iconStyle"></span>
-		<div class='inline' :style='statusClasses.iconStyle'>{{props.text}}</div>
+		<div  v-if="props.text != ''" class="white-space-nowrap m-1" :style='statusClasses.iconStyle'>{{props.text}}</div>
 	</div>
 </template>
 
